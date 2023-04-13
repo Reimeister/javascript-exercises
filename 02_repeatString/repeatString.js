ba436fef7303a9944525bf815db9597bb92fb9fe
Reimeister <128944245+Reimeister@users.noreply.github.com>
2023-04-13 16:01:25 -0500

    Create function to repeat string. Must add error for negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let repeatedString = "";
+  while (num > 0) {
+    repeatedString += string;
+    num--;
+  }
+  return repeatedString;
 };
 
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
+
+/* Repeat variable (string) n times, if n is a positive number. 
+If number is negative, return "ERROR". If number is 0, return empty string. */
